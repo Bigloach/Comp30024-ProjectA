69f3cc8e6fb1317f24d492d55e37dd330ca8f03d
little loach <lyrz2023@gmail.com>
2025-03-24 04:08:34 +0000

    implement dfs all cross jumps and most a* searc

--- search/search.py
+++ search/search.py
@@ -1,56 +1,74 @@
 import heapq
 from .core import CellState, Coord, Direction, MoveAction, BOARD_N
 
+DIRECTION = [
+    Direction.Up,
+    Direction.UpLeft,
+    Direction.UpRight,
+    Direction.Left,
+    Direction.Right,
+]
+
 def a_star_search(board:dict[Coord, CellState]):
     goals = get_goals(board) 
     red =  [cord for cord, state in board.items() if state == CellState.RED][0]
     h = calculate_h(board)
     distance = {red: 0}
-    visited = set()
-    min_path = []
     queue = []
     
-    heapq.heappush(queue, (0, red))
+    heapq.heappush(queue, (0, 0, red))
     
     while queue:
-        curr_cost, curr_pos = heapq.heappop(queue)
+        _, curr_cost, curr_pos = heapq.heappop(queue)
         
         if curr_pos in goals:
             return 
 
-        if curr_pos in visited:
-            continue
-        
-        visited.add(curr_pos)
+        if curr_pos not in distance or distance[curr_pos] > curr_cost:
+            distance[curr_pos] = curr_cost
 
-        valid_moves = get_valid_moves
-        for move in valid_moves:
-            next_pos, dir = move.coord, move._directions 
-            f = curr_cost + h[next_pos] + 1
-            if type(dir) == list:
-                
-                
-            heapq.heappush(queue, (f, next_pos))
-        
+            for move in get_valid_moves(board, curr_pos):
+                next_pos, dir = move.coord, move._directions 
+                next_cost = curr_cost + 1
+                f = next_cost + h[next_pos] 
+                heapq.heappush(queue, (f, next_cost, next_pos))
+
+    return None  
         
 
 def get_valid_moves(board, red):
     valid_moves = []
-    is_cross = True
-    for dir in Direction:
+
+    for dir in DIRECTION:
         move = red + dir
         if move in board and board[move] == CellState.LILY_PAD:
             valid_moves.append(MoveAction(move, dir))
         
-        if move in board and board[move] == CellState.BLUE:
-            cross_move = move + dir
-            if cross_move in board and board[cross_move] == CellState.LILY_PAD:
-                dir_list = []
-                dir_list.append(dir)
-                valid_moves.append((MoveAction(cross_move, dir_list)))
-    
-    return valid_moves
+    for dest, paths in get_cross_jumps(red, [], board, {}).items():
+        for path in paths:
+            valid_moves.append(MoveAction(dest, path))
 
+    return valid_moves
+        
+def get_cross_jumps(curr_pos, visited, board, result):
+    for dir in DIRECTION:
+        if (curr_pos + dir) in board and board[curr_pos + dir] == CellState.BLUE:
+            dest = curr_pos + 2 * dir 
+            if (dest in board and 
+                board[dest] == CellState.LILY_PAD and
+                dest not in visited):
+
+                visited.append(dest)
+                
+                if dest not in result:
+                    result[dest] = []
+                result[dest].append(visited[:])
+
+                get_cross_jumps(dest, visited, board, result)
+                visited.pop()
+
+    return result 
+                
 def calculate_h(board:dict[Coord, CellState]) -> dict[Coord, int]:
     man_distance = {}
     lily_pads = get_lily_pads(board)
